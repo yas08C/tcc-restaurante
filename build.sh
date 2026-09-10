edacebc376269542a8a995ec6e8d460d8fe3e133
yasmin <yayaaraujo1411@gmail.com>
2026-09-10 08:05:30 -0300

    cria superusuario automatico no deploy

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-# Script de build usado pelo Render.
 set -o errexit
 
 pip install -r requirements.txt
@@ -7,3 +6,17 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 
 python manage.py migrate
+
+python manage.py shell << EOF
+from django.contrib.auth import get_user_model
+import os
+User = get_user_model()
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL', '')
+if username and password and not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print('Superusuario criado:', username)
+else:
+    print('Nenhum superusuario novo criado (ja existe ou faltam variaveis).')
+EOF
