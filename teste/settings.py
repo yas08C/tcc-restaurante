67131af9f5aca5f651232ab73e254f100e56c933
yasmin <yayaaraujo1411@gmail.com>
2026-09-16 08:42:52 -0300

    Adiciona LOGGING para exibir tracebacks de erro 500 no console

--- teste/settings.py
+++ teste/settings.py
@@ -164,3 +164,23 @@ MAILERS = {
     },
 }
  
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+    },
+}
